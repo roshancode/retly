79a07a448d4e226857a236730f3f9d1cece72562
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-07-23 11:58:18 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
 	template: __dirname + '/app/index.html',
 	filename: 'index.html',
 	inject: 'body'
 });
+
+
 
 module.exports = {
 	entry: __dirname + '/app/index.js',
